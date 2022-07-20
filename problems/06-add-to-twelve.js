b455597725368cb6190624d237fcc9eeb1f1c989
gabriellaguerre <laguerre.g.g@gmail.com>
2022-07-20 19:47:57 -0400

    prob 6

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -11,12 +11,26 @@ addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
+//base case --> i = array.length
+//recursive case --> i > 0
+//recursive step --> i++
 
+function addToTwelve(arr, i = 0) {
+   if (i === arr.length) {
+    return false;
 
-function addToTwelve(arr) {
-  // Your code here
+   } else if (arr[i] + arr[i+1] === 12) {
+    return true;
+   }
+   i++
+  return addToTwelve(arr, i);
 }
 
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
